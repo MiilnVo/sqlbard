41942b44c2d5f0955d6e237a163347b54f2705de
MiilnVo <miilnvo@qq.com>
2022-07-15 10:14:18 +0800

    Feature: 添加Example模块

---
+++ example/src/main/java/com/miilnvo/sqlbard/example/SqlbardExampleApplication.java
@@ -0,0 +1,13 @@
+package com.miilnvo.sqlbard.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlbardExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlbardExampleApplication.class, args);
+	}
+
+}
---
+++ example/src/test/java/com/miilnvo/sqlbard/example/SqlbardExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miilnvo.sqlbard.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlbardExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
