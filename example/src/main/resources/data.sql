dbf45cd857786de30847738b4101a1df794738bc
MiilnVo <miilnvo@qq.com>
2022-07-22 15:52:15 +0800

    Test: 添加示例数据

--- example/src/main/resources/data.sql
+++ example/src/main/resources/data.sql
@@ -37,3 +37,43 @@ VALUES
 		'2022-07-19 14:16:01',
 	  '2022-07-19 14:16:03'
 	);
+
+INSERT INTO `m`.`user` (
+	`t_integer`,
+	`t_float`,
+	`t_double`,
+	`t_long`,
+	`t_char`,
+	`t_bytes`,
+	`t_string`,
+	`t_bigdecimal`,
+	`t_gender`,
+	`t_enabled`,
+	`t_year`,
+	`t_date`,
+	`t_time`,
+	`t_localdate`,
+	`t_localtime`,
+	`t_localdatetime`,
+	`t_timestamp`
+)
+VALUES
+	(
+		2,
+		0.01,
+		9.999,
+		121284102412,
+		'b',
+		0x38,
+		'hello',
+		1000,
+		'male',
+		1,
+		2022,
+		'2022-09-19',
+		'20:08:23',
+		'2022-09-19',
+		'20:08:45',
+		'2022-09-19 20:08:51',
+	  '2022-09-19 20:08:55'
+	);
